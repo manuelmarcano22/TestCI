6f16f2d72c310c56386e14ed28915f89dfb7eaeb
LRS <manuelmarcano22@yahoo.com>
2024-07-31 13:02:15 -0500

    requirements file

--- math_operations.py
+++ math_operations.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def add(a, b):
     return a + b
 
@@ -10,4 +13,10 @@ def multiply(a, b):
     return a * b
 
 
+
+def mean(numbers):
+  return np.mean(numbers)
+
 print(add(10, 5))
+
+print(np.mean([1,2,3]))
--- test_math_operations.py
+++ test_math_operations.py
@@ -1,4 +1,4 @@
-from math_operations import add, subtract, multiply
+from math_operations import add, subtract, multiply, mean
 import pytest
 
 
@@ -15,3 +15,14 @@ def test_subtract():
 
 def test_multiply():
     assert multiply(3, 5) == 15
+
+
+@pytest.mark.parametrize("numbers, expected", [
+  ([10, 20, 30], 20),
+  ([1.5, 2.5, 3.5], 2.5)
+])
+
+
+def test_mean(numbers, expected):
+  if numbers:
+    assert mean(numbers) == expected
